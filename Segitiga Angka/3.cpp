7eb18982257eb325e45873c4d8272fcc6dfc9481
freemann0 <tomhagen849@gmail.com>
2023-09-21 16:23:46 +0700

    Program Segitiga Angka

---
+++ Segitiga Angka/3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    int main() {
+
+        int n, k;
+
+        cout << "Input tinggi segitiga: "; cin >> n;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                k++;
+                cout << setw(3) << k << " ";
+            }
+            cout << endl;
+        }
+
+        return 0;
+    }
